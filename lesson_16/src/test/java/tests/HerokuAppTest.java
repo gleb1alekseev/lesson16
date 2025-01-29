573dd08e18d6b1b8d615c41628601074ea5c6f2f
g.alekseev <gleb1alekseev@gmail.com>
2025-01-29 23:13:30 +0300

    lesson16

--- lesson_16/src/test/java/tests/HerokuAppTest.java
+++ lesson_16/src/test/java/tests/HerokuAppTest.java
@@ -6,5 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HerokuAppTest {
+    // 1. Open browser site https://the-internet.herokuapp.com/login
+    // 2. Enter tomsmith for the username and SuperSecretPassword! for the password
+    // 3. Click on 'login' button
 
+    @Test
+    public void herokuLoginTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/login");
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.xpath("//*[@id=\"login\"]/button")).click();
+
+    }
 }
--- lesson_16/src/test/java/tests/SampleAppTest.java
+++ lesson_16/src/test/java/tests/SampleAppTest.java
@@ -6,5 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SampleAppTest {
+    // 1. Open browser site http://www.uitestingplayground.com/sampleapp
+    // 2. Enter any non-empty user name and `pwd` as password
+    // 3. Click on 'log in' button
 
+    @Test
+    public void SampleAppLoginTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.uitestingplayground.com/sampleapp");
+        driver.findElement(By.name("UserName")).sendKeys("Gleb");
+        driver.findElement(By.name("Password")).sendKeys("pwd");
+        driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+    }
 }
